f503fecd4146a779fb627476c9ac9cf4be364582
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-02-17 15:39:01 +0530

    Time: 4 ms (99.06%) | Memory: 43.7 MB (87.49%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        
+        //Created a frequency array of size 128
+        char []frq=new char[128];//default value of char is '\0'
+
+        for(int i=0;i<s.length();i++){
+            char st=s.charAt(i);
+            char tt=t.charAt(i);
+
+            int idx=(int)st;
+            if(frq[idx]=='\0'){
+                frq[idx]=tt;
+            }
+            else{
+                if(frq[idx]!=tt){
+                    return false;
+                }
+            }
+        }
+
+        for(int i=0;i<128;i++){
+            frq[i]='\0';
+        }
+
+        for(int i=0;i<t.length();i++){
+            char st=s.charAt(i);
+            char tt=t.charAt(i);
+
+            int idx=(int)tt;
+            if(frq[idx]=='\0'){
+                frq[idx]=st;
+            }
+            else{
+                if(frq[idx]!=st){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+
+
+    }
+}
