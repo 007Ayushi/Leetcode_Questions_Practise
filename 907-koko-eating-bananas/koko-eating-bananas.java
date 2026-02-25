6fb5f41a6ffe3e493f70ee16c628a55044d0976f
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-02-25 16:44:32 +0530

    Time: 27 ms (13.05%) | Memory: 47.8 MB (73.77%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+     //Binary Search
+     int mx=Integer.MIN_VALUE;
+     for(int i=0;i<piles.length;i++){
+        mx=Integer.max(mx,piles[i]);
+     }
+
+     int lo=1;
+     int hi=mx;
+
+     int ans=1;
+     while(lo<=hi){
+
+        int mid=lo+(hi-lo)/2;
+
+        int k=0;
+        for(int i=0;i<piles.length;i++){
+            k+=Math.ceil((double)piles[i]/mid);
+        }
+
+        if(k<=h){
+            ans=mid;
+            hi=mid-1;
+        }
+        else{
+            lo=mid+1;
+        }
+        
+     }
+     return ans;
+
+    }
+}
