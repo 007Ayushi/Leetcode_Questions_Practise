fad5d62ac545a7f904a62214943c44b62ded9040
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-03-06 15:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (69.35%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -6,9 +6,15 @@ class Solution {
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
 
-            if(nums[mid]==target)return mid;
-            else if(nums[mid]<target)lo=mid+1;
-            else if(nums[mid]>target)hi=mid-1;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                lo=mid+1;
+            }
+            else{
+                hi=mid-1;
+            }
         }
         return -1;
     }
