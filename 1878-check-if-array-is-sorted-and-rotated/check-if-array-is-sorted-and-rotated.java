72d7ea492c53fcbb3c894e5a0ef0e6c6401315fd
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-07 14:23:39 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (54.68%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -2,6 +2,7 @@ class Solution {
     public boolean check(int[] nums) {
 
      //circular Approch
+     //TC = O(n) and sc=O(1)
      int n=nums.length;
      int cnt=0;
      for(int i=0;i<nums.length;i++){
@@ -14,3 +15,5 @@ class Solution {
 
     }
 }
+//we are counting how many times the order breaks in the circular array
+//If is less than or equal to 1 then array is sorted and rotated.
