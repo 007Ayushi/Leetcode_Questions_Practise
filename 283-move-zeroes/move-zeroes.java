89edadab0341b312b084967ac02183e594776e59
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-10 14:38:07 +0530

    Time: 2 ms (91.11%) | Memory: 47.7 MB (80.69%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,15 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int non_zeroes=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[non_zeroes++]=nums[i];
+            }
+        }
+
+        for(int i=non_zeroes;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+}
