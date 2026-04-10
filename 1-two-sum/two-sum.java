9a86dfbc75fa01714b6dc0f80b73d3692c21b7a0
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-10 17:47:42 +0530

    Time: 6 ms (46.52%) | Memory: 46.5 MB (97.72%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -3,22 +3,38 @@ class Solution {
         
         //Tc=O(n^2) and sc=O(n)
         //Two Loop
-        int sum=0;
-        int idx=0;
-        int []ans=new int[2];
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i]+nums[j]==target){
-                    ans[0]=i;
-                    ans[1]=j;
-                     return new int[]{i,j};
-                }
+        //Method 1 - Brute force Approch using 2 For Loop
+        // int sum=0;
+        // int idx=0;
+        // int []ans=new int[2];
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=i+1;j<nums.length;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans[0]=i;
+        //             ans[1]=j;
+        //              return new int[]{i,j};
+        //         }
+        //     }
+        // }
+
+        // return ans;
+
+       //Second Approch - HashMap
+       //Tc=o(n) and sc=O(n)
+       HashMap<Integer,Integer> mp=new HashMap<>();
+       int []ans=new int[2];
+       for(int i=0;i<nums.length;i++){
+            int ele=target-nums[i];
+
+            if(!mp.containsKey(nums[i])){
+                mp.put(ele,i);
             }
-        }
-
-        return ans;
-
-
-        
+            else{
+                ans[0]=mp.get(nums[i]);
+                ans[1]=i;
+               
+            }
+       }
+         return ans;
     }
 }
