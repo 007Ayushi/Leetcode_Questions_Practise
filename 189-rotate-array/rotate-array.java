67b3045dc473dafce28d8aa4a138da8c700adb03
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-10 14:02:23 +0530

    Time: 4 ms (4.72%) | Memory: 64.2 MB (5.43%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,15 +1,13 @@
 class Solution {
     public void rotate(int[] nums, int k) {
 
+        //tc= O(n) and sc=O(n)
        //First way
-       
        int n=nums.length;
        k=k%n;
        ArrayList<Integer> ans=new ArrayList<>();
-       for(int i=0;i<nums.length;i++){
-            if(i>=n-k){
+       for(int i=n-k;i<nums.length;i++){
                 ans.add(nums[i]);
-            }
        }
 
        for(int i=0;i<n-k;i++){
@@ -23,3 +21,5 @@ class Solution {
        
     }
 }
+// This is a simple approach using extra space (O(n)).
+// But we can optimize it to O(1) using the reverse method
