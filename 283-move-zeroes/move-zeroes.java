e62f5e8ec5fa8da4afb35e01090745e5da1f6baa
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-10 14:54:39 +0530

    Time: 2 ms (91.11%) | Memory: 47.7 MB (64.46%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,15 +1,32 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        int non_zeroes=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=0){
-                nums[non_zeroes++]=nums[i];
+        //Method-1
+        //TC=O(N)+O(N)=O(2N) = O(N) and sc=O(1)
+        // int non_zeroes=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         nums[non_zeroes++]=nums[i];
+        //     }
+        // }
+
+        // for(int i=non_zeroes;i<nums.length;i++){
+        //     nums[i]=0;
+        // }
+
+        //Method-2
+
+        //tc=O(N) and O(1)
+        int i=0;
+        for(int j=0;j<nums.length;j++){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
+                i++;
             }
         }
 
-        for(int i=non_zeroes;i<nums.length;i++){
-            nums[i]=0;
-        }
+
     }
 }
