baf00e359b52bc4b887199a635946836436fbe16
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-04-26 21:59:37 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (70.39%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,25 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int lo=0,hi=nums.length-1;
 
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
+     //Applying Binary Search
+     //Time Complexity - O(logn) and space Complexity - O(1)
 
-            if(nums[mid]==target){
-                return mid;
-            }
-            else if(nums[mid]<target){
-                lo=mid+1;
-            }
-            else{
-                hi=mid-1;
-            }
+
+    int lo=0,hi=nums.length-1;
+     while(lo<=hi){
+        int mid=lo+(hi-lo)/2;
+
+        if(nums[mid]==target){
+            return mid;
         }
-        return -1;
+        else if(nums[mid]<target){
+            lo=mid+1;
+        }
+        else{
+            hi=mid-1;
+        }
+     }
+     return -1;
+
     }
 }
