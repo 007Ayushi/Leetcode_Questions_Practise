ea9d5faaecb2e742bf0ff15ce420f343f41a51df
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-05-16 11:57:52 +0530

    Time: 63 ms (7.41%) | Memory: 46.9 MB (11.51%) - LeetSync

--- 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
+++ 2190-count-common-words-with-one-occurrence/count-common-words-with-one-occurrence.java
@@ -1,33 +1,40 @@
 class Solution {
     public int countWords(String[] words1, String[] words2) {
         
+        HashMap<String,Integer> mp=new HashMap<>();
         int ans=0;
-        int cnt=0;
         for(int i=0;i<words1.length;i++){
-            String str=words1[i];
-            cnt=0;
-            
-            int cnt1=0;
-            for(int k=0;k<words1.length;k++){
-                if(words1[k].equals(str)){
-                    cnt1++;
+            String ele=words1[i];
+            if(mp.containsKey(ele)){
+               int frq=mp.get(ele);
+                mp.put(ele,frq+1);
+            }
+            else{
+                mp.put(ele,1);
+            } 
+        }
+
+        for(String val:mp.keySet()){
+            System.out.println(val+" "+mp.get(val));
+
+        }
+        for(String ele:mp.keySet()){
+
+           int frq_ele=mp.get(ele);
+            if(frq_ele==1){
+            int frq2=0;
+            for(int j=0;j<words2.length;j++){
+                if(ele.equals(words2[j])){
+                    frq2++;
                 }
             }
-            if(cnt1==1){
-                for(int j=0;j<words2.length;j++){
-                 if(words2[j].equals(str)){
-                    cnt++;
-                 }
-            }
-            if(cnt==1){
+            if(frq2==1 && frq_ele==1){
                 ans++;
             }
         }
 
-            }
-
-            
+        }
+      
         return ans;
-
     }
 }
