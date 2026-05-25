13b788224f83edeb22424a3e1ed0f48f3c889c1f
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-05-25 09:05:51 +0530

    Time: 20 ms (9.63%) | Memory: 47.2 MB (5.07%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,19 +1,43 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
 
-      char []ch1=s.toCharArray();
-      char []ch2=t.toCharArray();
+    //base case
+    if(s.length()!=t.length())return false;
+     char []ch=s.toCharArray();
+     char []ch1=t.toCharArray();
 
-      Arrays.sort(ch1);
-      Arrays.sort(ch2);
+     Arrays.sort(ch);
+     Arrays.sort(ch1);
 
-      if(ch1.length!=ch2.length)return false;
+     HashMap<Character,Integer> mp=new HashMap<>();
+     for(int i=0;i<ch.length;i++){
+        char ele=ch[i];
+        if(mp.containsKey(ele)){
+            int frq=mp.get(ele);
+            mp.put(ele,frq+1);
+        }
+        else{
+            mp.put(ele,1);
+        }
+     }   
 
-      for(int i=0;i<ch1.length;i++){
-            if(ch1[i]!=ch2[i]){
-                return false;
-            }
-      }
-      return true;  
+     for(int i=0;i<ch1.length;i++){
+        if(mp.containsKey(ch1[i])){
+            int frq=mp.get(ch1[i]);
+            mp.put(ch1[i],frq-1);
+        }
+     }
+
+     for(char ele:mp.keySet()){
+        if(mp.get(ele)!=0){
+            return false;
+        }
+     }
+     return true;
+
+
+
+
+
     }
 }
