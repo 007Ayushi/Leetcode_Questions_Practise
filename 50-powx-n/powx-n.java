016b960ee927594f3e2302dc24274923098a96a4
Ayushi Gupta <164901414+007Ayushi@users.noreply.github.com>
2026-06-05 16:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (70.98%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,19 +1,28 @@
 class Solution {
+
     public double myPow(double x, int n) {
-        if(n==0)return 1; 
-    
-        //if power(n) is negative then
-        if(n<0){
-        x=1/x;
-        n=-n;
+
+        long N = n;
+
+        if(N < 0){
+            x = 1 / x;
+            N = -N;
         }
 
-        return pow(x,n);
+        return pow(x, N);
     }
-    public double pow(double x,int n){
-        if(n==0)return 1;
-        double ans=pow(x,n/2);
-        if(n%2==0)return ans*ans;
-        else return ans*ans*x;
+
+    public double pow(double x, long n){
+
+        if(n == 0) return 1;
+
+        double ans = pow(x, n / 2);
+
+        if(n % 2 == 0){
+            return ans * ans;
+
+        } else {
+            return ans * ans * x;
+        }
     }
 }
